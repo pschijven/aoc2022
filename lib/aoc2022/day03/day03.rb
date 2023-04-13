6d0a330ab851fe0d72aa039f2a83aca633e6717b
Piet Schijven <piet.schijven@innoq.com>
2023-04-13 10:23:49 +0200

    solutions for day 3

---
+++ lib/aoc2022/day03/day03.rb
@@ -0,0 +1,42 @@
+class Day03
+
+  def initialize
+    # Initialize Hash of priorities
+    @priorities = initialize_priorities
+  end
+
+  def priority(content)
+    if is_valid(content)
+      compartment1 = content[0...(content.length / 2)].chars
+      compartment2 = content[(content.length / 2)..-1].chars
+      common = compartment1.intersection(compartment2).first
+
+      @priorities[common]
+    else
+      0
+    end
+  end
+
+  # We assume here that grouped_content is the content for a group of 3 elves (array of 3 strings)
+  def badge_priority(grouped_content)
+
+    if grouped_content.length == 3
+      common_char = grouped_content[0].chars.intersection(grouped_content[1].chars, grouped_content[2].chars).first
+      @priorities[common_char]
+    else
+      0
+    end
+  end
+
+  private
+
+  def initialize_priorities
+    lowercase = ("a".."z").to_h { |item| [item, item.ord - "a".ord + 1] }
+    uppercase = ("A".."Z").to_h { |item| [item, item.ord - "A".ord + 27] }
+    lowercase.merge uppercase
+  end
+
+  def is_valid(content)
+    content.length > 0 && content.length % 2 == 0 # Should be splittable in two equal parts
+  end
+end
---
+++ test/test_day03.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+require 'aoc2022/day03/day03'
+
+class Day03Test < Minitest::Test
+
+  def test_sample_input
+    day3 = Day03.new
+    assert_equal 16, day3.priority("vJrwpWtwJgWrhcsFMMfFFhFp")
+    assert_equal 38, day3.priority("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+    assert_equal 42, day3.priority("PmmdzqPrVvPwwTWBwg")
+    assert_equal 22, day3.priority("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
+    assert_equal 20, day3.priority("ttgJtRGJQctTZtZT")
+    assert_equal 19, day3.priority("CrZsJsPPZsGzwwsLwLmpwMDw")
+  end
+
+  def test_sample_input_part2
+    day3 = Day03.new
+    assert_equal(18, day3.badge_priority(["vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"]))
+    assert_equal(52, day3.badge_priority(["wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"]))
+  end
+
+  def test_day3_part1
+    day3 = Day03.new
+    result = File.readlines("data/day03", chomp: true)
+             .map { |line| day3.priority line }
+             .sum
+
+    assert_equal 7553, result
+  end
+
+  def test_day3_part2
+    day3 = Day03.new
+    result = File.readlines("data/day03", chomp: true)
+                 .each_slice(3)
+                 .map { |grouped_content| day3.badge_priority grouped_content }
+                 .sum
+
+    assert_equal 2758, result
+  end
+end
